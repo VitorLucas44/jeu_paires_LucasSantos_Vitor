852fd4641e078189b8632a60c60e21cfef04a378
VitorLucas44 <vitoriosolucas@hotmail.com>
2023-01-20 15:12:23 +0100

    htmlPseudo

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,32 @@
-let cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card"),
+timeTag = document.querySelector(".time b"),
+flipsTag = document.querySelector(".flips b"),
+refreshBtn = document.querySelector(".details button");
 
-let matched = 0;
-let cardOne, cardTwo;
+let maxTime = 20;
+let timeLeft = maxTime;
+let flips = 0;
+let matchedCard = 0;
 let disableDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
+
+function initTimer() {
+    if(timeLeft <= 0) {
+        return clearInterval(timer);
+    }
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+}
 
 function flipCard({target: clickedCard}) {
-    if(cardOne !== clickedCard && !disableDeck) {
+    if(!isPlaying) {
+        isPlaying = true;
+        timer = setInterval(initTimer, 1000);
+    }
+    if(clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+        flips++;
+        flipsTag.innerText = flips;
         clickedCard.classList.add("flip");
         if(!cardOne) {
             return cardOne = clickedCard;
@@ -14,22 +35,22 @@ function flipCard({target: clickedCard}) {
         disableDeck = true;
         let cardOneImg = cardOne.querySelector(".back-view img").src,
         cardTwoImg = cardTwo.querySelector(".back-view img").src;
-        PaireCards(cardOneImg, cardTwoImg);
+        matchCards(cardOneImg, cardTwoImg);
     }
 }
-function PaireCards(img1, img2) {
+
+function matchCards(img1, img2) {
     if(img1 === img2) {
-        matched++;
-        if(matched == 8) {
-            setTimeout(() => {
-                return MelangeCard();
-            }, 1000);
+        matchedCard++;
+        if(matchedCard == 6 && timeLeft > 0) {
+            return clearInterval(timer);
         }
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
         cardOne = cardTwo = "";
         return disableDeck = false;
     }
+
     setTimeout(() => {
         cardOne.classList.add("shake");
         cardTwo.classList.add("shake");
@@ -43,22 +64,32 @@ function PaireCards(img1, img2) {
     }, 1200);
 }
 
-function MelangeCard() {
-    matched = 0;
-    disableDeck = false;
+function shuffleCard() {
+    timeLeft = maxTime;
+    flips = matchedCard = 0;
     cardOne = cardTwo = "";
+    clearInterval(timer);
+    timeTag.innerText = timeLeft;
+    flipsTag.innerText = flips;
+    disableDeck = isPlaying = false;
+
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-    cards.forEach((card, i) => {
+
+    cards.forEach((card, index) => {
         card.classList.remove("flip");
         let imgTag = card.querySelector(".back-view img");
-        imgTag.src = `images/img-${arr[i]}.png`;
+        setTimeout(() => {
+            imgTag.src = `images/img-${arr[index]}.png`;
+        }, 500);
         card.addEventListener("click", flipCard);
     });
 }
 
-MelangeCard();
-    
+shuffleCard();
+
+refreshBtn.addEventListener("click", shuffleCard);
+
 cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
